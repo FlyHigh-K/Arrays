4aff45de728b05c6495ad7e60daa9f934a58be74
FlyHigh-K <102417687+FlyHigh-K@users.noreply.github.com>
2022-04-14 18:05:13 +0100

    arrays part 3

---
+++ arrays2.java
@@ -0,0 +1,7 @@
+class arrays2 {
+  public static void main(String[] args) {
+    int[] data ={5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100};
+    for (int i=0;i<=19;i++)
+    System.out.println(data[i]);
+  }
+}
