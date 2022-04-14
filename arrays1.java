14c7ca9939c358b57d37b168f76655b99a98076a
FlyHigh-K <102417687+FlyHigh-K@users.noreply.github.com>
2022-04-14 17:58:42 +0100

    arrays part 2

---
+++ arrays1.java
@@ -0,0 +1,9 @@
+class arrays1{
+  public static void main(String []args){
+    int[] timestable = new int[20];
+    for (int i=0; i<20;i=i+1){
+      timestable[i] = i;
+    }
+     System.out.println(timestable[5]);
+  }
+}
