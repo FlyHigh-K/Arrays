3c5eb695a3f2cf51d9e63c86f16a58c056b434e5
FlyHigh-K <102417687+FlyHigh-K@users.noreply.github.com>
2022-04-16 13:00:41 +0100

    Filling odd numbers in an array

---
+++ Fillingoddvaluesinanarray.java
@@ -0,0 +1,14 @@
+class Fillingoddvaluesinanarray {
+  public static void main(String[] args) {
+    int[] data = new int[50];
+    int count = 0;
+    for (int i = 1;i <= 100; i=i+2){
+      data[count] = i;
+      count = count+1;
+     //  System.out.println(data[count]);
+    }
+    for (int i= 0; i<=49; i++){
+      System.out.println(data[i]);
+    }
+  }
+}
