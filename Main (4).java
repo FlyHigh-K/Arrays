8c524540639589ad2466661057a3aeb3f1f224ca
FlyHigh-K <102417687+FlyHigh-K@users.noreply.github.com>
2022-04-23 12:40:03 +0100

    Print all numbers divisible by 5 from 1-100

---
+++ Main (4).java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    int[] data = new int[101];
+    for (int i=1;i<=100;i++){
+      data[i] =i;
+     // System.out.println(data[i]);
+
+
+if (data[i]%5==0){
+  System.out.println(data[i]);
+}
+
+      
+    }
+  }
+}
