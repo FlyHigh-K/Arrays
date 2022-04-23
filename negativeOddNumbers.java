a33ffc00a04fd828d2c450fcfe0aabe822629c55
FlyHigh-K <102417687+FlyHigh-K@users.noreply.github.com>
2022-04-23 12:04:37 +0100

    Negative Off Numbers

---
+++ negativeOddNumbers.java
@@ -0,0 +1,13 @@
+class negativeOddNumbers{
+  public static void main(String[] args) {
+    int[] data =  new int[51];
+    int count = 0;
+    for (int i=-1; i>=-100; i=i-2){
+      data[count] = i;
+      count=count+1;
+    }
+    for (int i=0;i<=49;i++){
+    System.out.println(data[i]); 
+    }
+  }
+}
